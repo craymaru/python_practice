69600a69d26e947c231dbf7bed64ab1ebf5c4947
Cray <craymaru@gmail.com>
2020-04-02 02:53:17 +0900

    first commit

---
+++ lesson.py
@@ -0,0 +1,25 @@
+s = ("""
+AAA
+BBB
+CCC
+DDD
+""")
+
+# with open ("text.txt", "w") as f:
+#     f.write(s)
+3
+# with open ("text,txt", "r") as f:
+#     # print(f.read())
+#     # while True:
+#     #     chunk = 5
+#     #     line = f.readline(chunk)
+#     #     print(line)
+#     #     if not line:
+#     #         break
+#
+#     print(f.tell())
+#     print(f.read(1))
+
+
+with open ("text.txt", "w") as f:
+    pass
