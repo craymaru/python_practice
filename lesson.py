a41f8d3951e8802689ba4dc24488872bd614faf1
Cray <craymaru@gmail.com>
2020-04-03 05:22:54 +0900

    Updated

--- lesson.py
+++ lesson.py
@@ -1,5 +1,35 @@
 import csv
 
+class Person:
+    def say_something(self):
+        print('hello')
+
+class Animal:
+     def say_something(self):
+         print('くぁwせdrftgyふじこlp')
+
+person = Person()
+person.say_something()
+
+animal = Animal()
+animal.say_something()
+
+class Person:
+    def say_something(self):
+        print('hello')
+
+class Animal:
+     def say_something(self):
+         print('くぁwせdrftgyふじこlp')
+
+person = Person()
+person.say_something()
+
+animal = Animal()
+animal.say_something()
+
+
+
 with open("test.csv", "w") as csv_file:
     fieldnames = ["Name", "Count"]
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
