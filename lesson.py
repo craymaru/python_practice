f213c63a729e766c576f6a9121f8ca5bbb3dda2f
Cray <craymaru@gmail.com>
2020-04-05 00:15:22 +0900

    Updated

--- lesson.py
+++ lesson.py
@@ -1,13 +1,44 @@
-import subprocess
+import datetime
 
-subprocess.run(["ls", "-al"])
+print("datetime:")
 
-subprocess.run("ls", shell=True, check=True)
+now = datetime.datetime.now()
+print(now)
+print(now.isoformat())
+print(now.strftime("%d/%m/%y-%H%M%S%f"))
 
+today = datetime.date.today()
+print(today)
+print(today.isoformat())
+print(today.strftime("%d/%m/%y"))
 
-p1 = subprocess.Popen(["ls", "-al"], stdout=subprocess.PIPE)
-p2 = subprocess.Popen(
-    ["grep", "test"], stdin=p1.stdout, stdout=subprocess.PIPE)
-p1.stdout.close()
-output = p2.communicate()[0]
-print(output)
+t = datetime.time(hour=1, minute=10, second=5, microsecond=100)
+print(t)
+print(t.isoformat())
+print(t.strftime("%H_%M_%S_%f"))
+
+print(now)
+# d = datetime.timedelta(weeks=1)
+d = datetime.timedelta(days=365)
+# d = datetime.timedelta(hours=1)
+# d = datetime.timedelta(minutes=1)
+# d = datetime.timedelta(second=1)
+# d = datetime.timedelta(microseconds=1)
+print(now - d)
+
+import time
+print("time:")
+time.sleep(1)
+print(time.time())
+
+import os
+import shutil
+
+file_name = "test.txt"
+if os.path.exists(file_name):
+    shutil.copy(file_name, "{}.{}".format(
+        file_name, now.strftime("%Y_%m_%d_%H_%M_%S")
+    ))
+
+with open(file_name, "w") as f:
+    f.write("test")
