3bf2772017ecbc947cc7ba73fa68a58654e70abf
Cray <craymaru@gmail.com>
2020-04-04 22:44:58 +0900

    Updated

---
+++ lesson-102-subprocess.py
@@ -0,0 +1,13 @@
+import subprocess
+
+subprocess.run(["ls", "-al"])
+
+subprocess.run("ls", shell=True, check=True)
+
+
+p1 = subprocess.Popen(["ls", "-al"], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(
+    ["grep", "test"], stdin=p1.stdout, stdout=subprocess.PIPE)
+p1.stdout.close()
+output = p2.communicate()[0]
+print(output)
