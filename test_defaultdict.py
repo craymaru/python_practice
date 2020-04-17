03ff356ea8fed401404839ee7e0ed218c936d328
Cray <craymaru@gmail.com>
2020-04-17 13:29:29 +0900

    test

---
+++ test_defaultdict.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+dd = defaultdict(int)
+print(dd)
+
+dd["Meow"] = 1
+print(dd)
+
+print(dd["unko"])
+print(dd)
+
+print("-" * 20)
+
+d = {}
+
+d["Meow"] = 1
+print(d)
+d["Unko"]
---
+++ test_get.py
@@ -0,0 +1,7 @@
+d = {"Meow": 1, "Gao": 999}
+
+print(d.get)
+print(type(d.get))
+
+print(d.get("Meow"))
+print(type(d.get("Meow")))
